1dfb52b13a44aa3db59a031f074506ee8b2a946d
Samuel T. Wallace <samueltwallace@gmail.com>
2020-05-16 15:39:17 -0400

    finished p4.jl

--- p4.jl
+++ p4.jl
@@ -4,16 +4,12 @@ N=24
 h=2*π/N
 x = h .* ([i for i=1:N])
 D = zeros(N,N)
-for i=1:(N-1)
-	for j=1:i
-		if i % 2 ==0
-			D[N - i + j, j] = 1/2* cot(j*h/2)
-		else
-			D[N - i + j, j] = -1/2* cot(j*h/2)
-		end
-	end
+
+col = [ 0.5*(-1)^j*cot(j*h/2) for j=1:(N-1)]
+col = vcat([0],col)
+for i=N:-1:1
+	D[:,i] = vcat(col[N-i+2:N],col[1:N-i+1])
 end
-D = D - D'
 
 u = exp.(sin.(x))
 uprime = cos.(x) .* u
