b123b36e839ce71ca6c3fafbc82dea6e5e9b78f0
Samuel T. Wallace <samueltwallace@gmail.com>
2020-05-27 15:17:56 -0400

    Having another wrong dimension problem...

--- p16.jl
+++ p16.jl
@@ -8,17 +8,18 @@ include("cheb.jl")
 N = 24
 (D,x) = cheb(N)
 y=x
-xx = repeat(x, size(y)...)
-yy = repeat(y, size(x)...)
+xx = repeat(x[2:N], size(y[2:N])...)
+yy = repeat(y[2:N], size(x[2:N])...)
 
 f = 10*sin.(8*xx .* (yy .- 1))
 D2 = (D^2)[2:N,2:N]
-L = kron(I,D2) + kron(D2,I)
+identity = Diagonal([1 for i=1:(N-1)])
+L = kron(identity,D2) + kron(D2,identity)
 u = L\f
 
 uu = zeros(N+1, N+1)
 uu[2:N,2:N] = reshape(u, N-1, N-1)
-pyplot()
+gr()
 plot(xx,yy,uu,st=:surface)
-savefig("p16.jl")
+savefig("p16.png")
 println("Done!")
