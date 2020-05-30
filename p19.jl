089b10712613859d4f8d7b5189e01442cffcabc8
Samuel T. Wallace <samueltwallace@gmail.com>
2020-05-30 15:33:50 -0400

    Tested numbers and plotted various time values, all look right, the plotting doesn't look right though...

--- p19.jl
+++ p19.jl
@@ -6,7 +6,7 @@ N = 80
 x = cos.(π*(0:N)/N)
 dt = 8/(N^2)
 
-v = exp.(-200*x.^2)
+v = exp.(-200*(x.^2))
 vold = exp.(-200*(x.-dt).^2)
 
 tmax = 4
@@ -25,7 +25,7 @@ for i=1:nplots
 		w = chebfft(chebfft(v))
 		w[1] = 0
 		w[N+1] = 0
-		vnew = 2*v - vold + dt^2*w
+		vnew = 2*v - vold + (dt^2)*w
 		global vold = v
 		global v = vnew
 	end
@@ -34,6 +34,6 @@ for i=1:nplots
 end
 
 gr()
-plot(reverse(x),tdata, plotdata, st=:wireframe, camera=(10,70),zaxis=(-2,2))
+plot(reverse(x),tdata, reverse(plotdata, dims=2), st=:wireframe, camera=(10,70),zaxis=(-2,2))
 savefig("p19.png")
 println("Done!")
