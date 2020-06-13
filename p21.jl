45039856fdc0a14cbad558f2bbac2b22f1382e79
Samuel T. Wallace <samueltwallace@gmail.com>
2020-06-12 23:02:22 -0400

    finished p21, for real this time, no complex eigenvalues.

--- p21.jl
+++ p21.jl
@@ -9,8 +9,8 @@ x = h*(1:N)
 D = zeros(N,N)
 
 
-col = [ 0.5*(-1)^j*cot(j*h/2) for j=1:(N-1)]
-col = vcat([0],col)
+col = [- (-1)^j * csc(j*h/2).^2 /2 for j =1:(N-1)]
+col = [- π^2/(3*h^2) - 1/6; col; - π^2/(3*h^2) - 1/6]
 for i=N:-1:1
 	D[:,i] = vcat(col[N-i+2:N],col[1:N-i+1])
 end
@@ -21,7 +21,8 @@ data = zeros(11,length(qq))
 for i = 1:length(qq)
 	q = qq[i]
 	e = eigvals(-D + 2*q*Diagonal(cos.(2*x)))
-	global data[:,i] = sort(imag.(e))[1:11]
+	e = sort!(real.(e))
+	global data[:,i] = e[1:11]
 end
 
 pyplot()
